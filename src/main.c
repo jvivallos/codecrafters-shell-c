caa38ff2d6d0e01d816d2f0a1a5e947899e0dfb6
Javier Vivallos <jvivallos@gmail.com>
2025-05-05 19:43:20 -0400

    uncomment

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
-  // printf("$ ");
+  printf("$ ");
 
   // Wait for user input
   char input[100];
