39247333d74443e2dba917a4ed7ac870bf2f6363
Javier Vivallos <jvivallos@gmail.com>
2025-05-05 19:58:12 -0400

    invalid

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int replace_new_line_null_terminator(char *buffer, size_t size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    if (buffer[i] == '\n')
+    {
+      buffer[i] = '\0';
+    }
+  }
+
+  return 0;
+}
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
@@ -12,6 +25,7 @@ int main(int argc, char *argv[])
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  replace_new_line_null_terminator(input, 100);
   printf("%s: command not found\n", input);
   return 0;
 }
