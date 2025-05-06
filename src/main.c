0194542dcd92b380c766270583e6600062f472a3
Javier Vivallos <jvivallos@gmail.com>
2025-05-05 20:33:11 -0400

    invalid

--- src/main.c
+++ src/main.c
@@ -21,6 +21,22 @@ int is_exit_command(char *command, size_t size)
   return strcmp(command, "exit 0") == 0;
 }
 
+void execute_echo(char *command, size_t size)
+{
+  char *text_start = command + 5; // jumps echo space
+  printf("%s\n", text_start);
+}
+
+int parse_command(char *command, size_t size)
+{
+  if (strncmp(command, "echo ", 5) == 0)
+  {
+    execute_echo(command, size);
+    return 1;
+  }
+  return 0;
+}
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
@@ -39,7 +55,10 @@ int main(int argc, char *argv[])
     {
       return 0;
     }
-    printf("%s: command not found\n", input);
+    if (!parse_command(input, 100))
+    {
+      printf("%s: command not found\n", input);
+    }
   }
 
   return 0;
